1155e34bf6f88a38d0d34eb70aa39156ef750176
githubjenkinsmaven <riteshrsg11@gmail.com>
2021-12-16 12:45:04 +0530

    ADDING FRAMEWORK CLASSES

---
+++ src/test/java/MAVEN/PROJECT_MANAGER/Framework.java
@@ -0,0 +1,15 @@
+package MAVEN.PROJECT_MANAGER;
+
+public class Framework {
+public void main_1() {
+		
+		System.out.println("FRAMEWORK");
+		System.out.println("FRAMEWORK_1");
+		System.out.println("FRAMEWORK_2");
+		System.out.println("FRAMEWORK_3");
+		
+		
+	}
+
+
+}
